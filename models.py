ac94a2d45d7a2476b6ea6fcb662ef9fa2b693b4f
Aldemitar <aldemarcatanocardona@gmail.com>
2025-04-07 22:31:32 -0500

    Se agrego el archivo de models.py con todos los 2 modelos que se presentan

---
+++ models.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class ElectricVehicle(BaseModel):
+    brand: str
+    model: str
+    year: int
+    battery_capacity_kwh: float
+    range_km: float
+    charger_type: str
+
+class ElectricVehicleWithId(ElectricVehicle):
+    id: int
+
+class UpdateElectricVehicle(BaseModel):
+    model: Optional[str] = None
+    year: Optional[int] = None
+    battery_capacity_kwh: Optional[float] = None
+    range_km: Optional[float] = None
+    charger_type: Optional[str] = None
+
+class VehicleWithBrand(BaseModel):
+    brand: str
+    model: str
+
+class ElectricityPrice(BaseModel):
+    city: str
+    electricity_type: str
+    electricity_price: float
+    date: str
+
+class ElectricityPriceWithId(ElectricityPrice):
+    id: int
+
+class UpdateElectricityPrice(BaseModel):
+    electricity_type: Optional[str] = None
+    electricity_price: Optional[float] = None
+    date: Optional[str] = None
