fb04a61ac021ff32ce148b0fe7da27d95b27bf99
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-12 13:10:29 -0500

    Se inicio el main mediante el primer endpoint para crear un vehiculo

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from fastapi import FastAPI
-from models import Vehiculo, Bateria
-from vehiculos_electricos_aldemar.operations.operations_db import crear_vehiculo_con_bateria, obtener_vehiculos
-from database import init_db
+from contextlib import asynccontextmanager
+from fastapi import FastAPI, Depends, HTTPException, status
+from utils.connection_db import init_db, get_session
+from data.models import Vehiculo
+from data.schemas import VehiculoCreateForm, VehiculoRead
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+from typing import List
+from operations.operations_db import (
+    crear_vehiculo_db
+)
 
-app = FastAPI(title="API Vehículos Eléctricos")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
 
-init_db()
+app = FastAPI(lifespan=lifespan)
 
-@app.post("/vehiculos/")
-def crear_vehiculo(vehiculo: Vehiculo, bateria: Bateria):
-    return crear_vehiculo_con_bateria(vehiculo, bateria)
-
-@app.get("/vehiculos/")
-def listar_vehiculos():
-    return obtener_vehiculos()
+@app.post("/vehiculos/form", response_model=VehiculoRead, status_code=status.HTTP_201_CREATED, tags=["Vehículos"])
+async def crear_vehiculo_form(vehiculo: VehiculoCreateForm = Depends(), session: AsyncSession = Depends(get_session)):
+    vehiculo_dict = vehiculo.__dict__
+    vehiculo_create = VehiculoCreateForm(**vehiculo_dict)
+    return await crear_vehiculo_db(vehiculo_create, session)
