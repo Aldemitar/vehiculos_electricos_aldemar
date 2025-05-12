9dcc28c1c4c739e834cdd0587ca73aacb04dbf36
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-12 13:33:25 -0500

    Se agrego las operaciones para poder eliminar vehículos en la base de datos

--- operations/operations_db.py
+++ operations/operations_db.py
@@ -1,5 +1,6 @@
 from sqlalchemy.future import select
 from data.models import Vehiculo
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 async def crear_vehiculo_db(vehiculo_create, session: AsyncSession):
@@ -13,3 +14,14 @@ async def obtener_vehiculos_db(session: AsyncSession):
     result = await session.execute(select(Vehiculo))
     vehiculos = result.scalars().all()
     return vehiculos
+
+async def eliminar_vehiculo_db(vehiculo_id: int, session: AsyncSession):
+    result = await session.execute(select(Vehiculo).where(Vehiculo.id == vehiculo_id))
+    vehiculo = result.scalar_one_or_none()
+
+    if vehiculo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Vehículo no encontrado.")
+
+    await session.delete(vehiculo)
+    await session.commit()
+    return {"mensaje": f"Vehículo con ID {vehiculo_id} eliminado correctamente."}
