ad4cb0e5d688b7004cb0a94ee4fb69c276694c64
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-12 12:57:40 -0500

    Se actualizó la conexión a la base de datos por problemas con el servidor pero ya es funcional

--- utils/connection_db.py
+++ utils/connection_db.py
@@ -6,13 +6,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
-DB_DRIVER = os.getenv("DB_DRIVER")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-
-DATABASE_URL = f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
