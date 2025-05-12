146c7cc07ad13c3ffa89112ab3b24d3716445ad4
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-12 12:15:16 -0500

    Se actualizó la conexión a la base de datos tomando en cuenta el render

--- utils/connection_db.py
+++ utils/connection_db.py
@@ -1,7 +1,26 @@
-from sqlmodel import SQLModel, create_engine
+import os
+from dotenv import load_dotenv
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./vehiculos.db"
-engine = create_engine(DATABASE_URL, echo=True)
+load_dotenv()
+DB_DRIVER = os.getenv("DB_DRIVER")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_NAME = os.getenv("DB_NAME")
 
-def init_db():
-    SQLModel.metadata.create_all(engine)
+DATABASE_URL = f"{DB_DRIVER}://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+engine: AsyncEngine = create_async_engine(DATABASE_URL, echo=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session():
+    async with async_session() as session:
+        yield session
