ff5d9151b4159e2757d57b7d1ecbd13269150465
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-12 11:25:26 -0500

    Se modifico el main.py para agregar las operaciones con la base de datos de momento local

--- main.py
+++ main.py
@@ -1,58 +1,16 @@
-from fastapi import FastAPI, HTTPException
-from typing import List
-from operations import *
-from models import *
+from fastapi import FastAPI
+from models import Vehiculo, Bateria
+from vehiculos_electricos_aldemar.operations.operations_db import crear_vehiculo_con_bateria, obtener_vehiculos
+from database import init_db
 
-app = FastAPI()
+app = FastAPI(title="API Vehículos Eléctricos")
 
-@app.post("/electric-vehicles/", response_model=ElectricVehicleWithId)
-async def create_electric_vehicle(vehicle: ElectricVehicle):
-    return create_new_vehicle(vehicle)
+init_db()
 
-@app.get("/electric-vehicles/", response_model=List[ElectricVehicleWithId])
-async def get_all_electric_vehicles():
-    return read_all_electric_vehicles()
+@app.post("/vehiculos/")
+def crear_vehiculo(vehiculo: Vehiculo, bateria: Bateria):
+    return crear_vehiculo_con_bateria(vehiculo, bateria)
 
-@app.get("/electric-vehicles/filtered/", response_model=List[ElectricVehicleWithId])
-async def get_filtered_electric_vehicles(brand: str = None, year: int = None, charger_type: str = None):
-    return filter_electric_vehicles(brand, year, charger_type)
-
-@app.get("/electric-vehicles/search/", response_model=List[ElectricVehicleWithId])
-async def search_electric_vehicles(year: int = None, charger_type: str = None):
-    return search_electric_vehicles_by_criteria(year, charger_type)
-
-@app.put("/electric-vehicles/{brand}/{model}", response_model=ElectricVehicleWithId)
-async def update_electrics_vehicle(brand: str, model: str, vehicle_update: UpdateElectricVehicle):
-    updated_vehicle = update_electric_vehicle(brand, model, vehicle_update)
-    if updated_vehicle is None:
-        raise HTTPException(status_code=404, detail="Vehículo eléctrico no encontrado")
-    return updated_vehicle
-
-@app.delete("/electric-vehicles/{brand}/{model}", response_model=VehicleWithBrand)
-async def delete_electrics_vehicle(brand: str, model: str):
-    deleted_vehicle = delete_electric_vehicle(brand, model)
-    if deleted_vehicle is None:
-        raise HTTPException(status_code=404, detail="Vehículo eléctrico no encontrado")
-    return deleted_vehicle
-
-@app.post("/electricity-prices/", response_model=ElectricityPriceWithId)
-async def create_electricity_price(price: ElectricityPrice):
-    return create_new_electricity_price(price)
-
-@app.get("/electricity-prices/", response_model=List[ElectricityPriceWithId])
-async def get_all_electricity_prices():
-    return read_all_electricity_prices()
-
-@app.put("/electricity-prices/{city}/{electricity_type}", response_model=ElectricityPriceWithId)
-async def update_electricitys_price(city: str, electricity_type: str, price_update: UpdateElectricityPrice):
-    updated_price = update_electricity_price(city, electricity_type, price_update)
-    if updated_price is None:
-        raise HTTPException(status_code=404, detail="Precio de electricidad no encontrado")
-    return updated_price
-
-@app.delete("/electricity-prices/{city}/{electricity_type}", response_model=DeleteElectricityPrice)
-async def delete_electricitys_price(city: str, electricity_type: str):
-    deleted_price = delete_electricity_price(city, electricity_type)
-    if deleted_price is None:
-        raise HTTPException(status_code=404, detail="Precio de electricidad no encontrado")
-    return deleted_price
+@app.get("/vehiculos/")
+def listar_vehiculos():
+    return obtener_vehiculos()
