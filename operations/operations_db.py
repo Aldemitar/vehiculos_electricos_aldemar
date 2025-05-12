a372f8e421f63331ac1b6daa0bde2745e0005686
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-12 13:09:49 -0500

    Se agrego la operación para crear vehiculos en la base de datos

--- operations/operations_db.py
+++ operations/operations_db.py
@@ -1,17 +1,10 @@
-from sqlmodel import Session, select
-from models import Vehiculo, Bateria
-from utils.connection_db import engine
+from sqlalchemy.future import select
+from data.models import Vehiculo
+from sqlalchemy.ext.asyncio import AsyncSession
 
-def crear_vehiculo_con_bateria(vehiculo: Vehiculo, bateria: Bateria):
-    with Session(engine) as session:
-        session.add(vehiculo)
-        session.flush()
-        bateria.vehiculo_id = vehiculo.id
-        session.add(bateria)
-        session.commit()
-        session.refresh(vehiculo)
-        return vehiculo
-
-def obtener_vehiculos():
-    with Session(engine) as session:
-        return session.exec(select(Vehiculo)).all()
+async def crear_vehiculo_db(vehiculo_create, session: AsyncSession):
+    vehiculo = Vehiculo(**vehiculo_create.dict())
+    session.add(vehiculo)
+    await session.commit()
+    await session.refresh(vehiculo) 
+    return vehiculo
