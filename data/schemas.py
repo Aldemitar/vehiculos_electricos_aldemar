8f7d204c37c9509dbe920730ae930f8e10f45062
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-12 13:54:36 -0500

    Se agrego el schema para poder actualizar vehículos buscados por su ID en la base de datos

--- data/schemas.py
+++ data/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from fastapi import Form
 from data.models import MarcaVehiculo
+from typing import Optional
 
 class VehiculoBase(BaseModel):
     marca: MarcaVehiculo
@@ -25,3 +26,14 @@ class VehiculoRead(VehiculoBase):
     id: int
     class Config:
         orm_mode = True
+
+class VehiculoUpdateForm:
+    def __init__(
+        self,
+        marca: Optional[MarcaVehiculo] = Form(None),
+        modelo: Optional[str] = Form(None),
+        año: Optional[int] = Form(None)
+    ):
+        self.marca = marca
+        self.modelo = modelo
+        self.año = año
